11be222c661453f14ed43077fb2b240c2eee994f
romanmeghi <165105998+romanmeghi@users.noreply.github.com>
2024-05-01 23:45:13 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculate() {
+    var displayValue = document.getElementById('display').value;
+    var result = eval(displayValue);
+    document.getElementById('display').value = result;
+  }
+  
